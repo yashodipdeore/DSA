8145f736e40274856842e8608fd3ad5627458a34
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-02 17:12:19 +0530

    Array implimantation is completed

---
+++ S5_MasteringArray/app.js
@@ -0,0 +1,46 @@
+//SECTION - Array Implimentation
+//let arr = [];
+
+// arr.push(10);
+// arr.push('Yash');
+// arr.push(30);
+// arr.push(true);
+// arr.push(50);
+
+// console.log(arr);
+// console.log(typeof arr);
+
+//---------------------------
+
+let arr = new Array(3);
+arr[0] = 1;
+arr[1] = 2;
+arr[2] = 3;
+arr[3] = 4;
+arr[4] = 5;
+
+console.log(arr);
+console.log(typeof arr);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
