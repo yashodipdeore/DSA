13a0369a8e7728883c65c9ea519204e8ac85656f
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-03 18:07:46 +0530

    Right rotation by 1 element

--- S6_AdvancedArray/app.js
+++ S6_AdvancedArray/app.js
@@ -1,10 +1,25 @@
 //SECTION - 1. left rotation by 1 element
 
+// let arr = [1, 2, 3, 4, 5];
+
+// let firstEl = arr[0];
+// for (let i = 0; i < arr.length - 1; i++) {
+//   arr[i] = arr[i + 1];
+// }
+// arr[(arr.length - 1)] = firstEl;
+// console.log(arr);
+
+
+
+//SECTION - 1. Right rotation by 1 element
+
 let arr = [1, 2, 3, 4, 5];
 
-let firstEl = arr[0];
-for (let i = 0; i < arr.length; i++) {
-  arr[i] = arr[i + 1];
+let lastEl = arr[arr.length - 1];
+for (let i = arr.length - 1; i > 0; i--) {
+  arr[i] = arr[i - 1];
 }
-arr[(arr.length - 1)] = firstEl;
+
+arr[0] = lastEl;
 console.log(arr);
+
