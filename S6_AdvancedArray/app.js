7b9f6578c1bc4ab3858dd22044d7855f00837deb
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-03 19:11:04 +0530

    left and right rotation by k element

--- S6_AdvancedArray/app.js
+++ S6_AdvancedArray/app.js
@@ -11,15 +11,51 @@
 
 
 
-//SECTION - 1. Right rotation by 1 element
+//SECTION - 2. Right rotation by 1 element
 
+// let arr = [1, 2, 3, 4, 5];
+
+// let lastEl = arr[arr.length - 1];
+// for (let i = arr.length - 1; i > 0; i--) {
+//   arr[i] = arr[i - 1];
+// }
+
+// arr[0] = lastEl;
+// console.log(arr);
+
+
+
+//SECTION - 3. left and right rotation by k element
+
+// let arr = [1, 2, 3, 4, 5];
+
+// let n = 3;
+// for (let i = 0; i < n; i++) {
+//   let lastEl = arr[arr.length - 1];
+//   for (let i = arr.length - 1; i > 0; i--) {
+//     arr[i] = arr[i - 1];
+//   }
+//   arr[0] = lastEl;
+// };
+
+// console.log(arr);
+
+
+//SECTION - 4. left and right rotation by k element
 let arr = [1, 2, 3, 4, 5];
+let n = 5;
+let k = n % arr.length;
 
-let lastEl = arr[arr.length - 1];
-for (let i = arr.length - 1; i > 0; i--) {
-  arr[i] = arr[i - 1];
-}
+for (let i = 0; i < k; i++) {
+  let firstEl = arr[0];
+  for (let i = 0; i < arr.length - 1; i++) {
+    arr[i] = arr[i + 1];
+  }
+  arr[arr.length - 1] = firstEl;
+};
 
-arr[0] = lastEl;
 console.log(arr);
+
+
+
 
