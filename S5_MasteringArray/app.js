6a87fb7f3a0ca5b6f3a4434b38f2147d85f7d2bc
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-03 16:25:50 +0530

    added reverse array without extr space

--- S5_MasteringArray/app.js
+++ S5_MasteringArray/app.js
@@ -103,19 +103,37 @@ const prompt = promptSync();
 //------------------------------------------------
 
 //SECTION - Reverse array with extra space
-let arr = [10, 20, 30, 40, 50];
-let temp = new Array(arr.length);
+// let arr = [10, 20, 30, 40, 50];
+// let temp = new Array(arr.length);
 
-let j = 0;
-for (let i = arr.length - 1; i >= 0; i--) {
-  temp[j] = arr[i];
-  j++;
-};
+// let j = 0;
+// for (let i = arr.length - 1; i >= 0; i--) {
+//   temp[j] = arr[i];
+//   j++;
+// };
+
+// console.log(arr);
+// console.log(temp);
+
+
+//------------------------------------------------
+//------------------------------------------------
+
+//SECTION - Reverse array without extra space
+let arr = [10, 20, 30, 40, 50];
+
+let i = 0;
+let j = arr.length - 1;
+
+while (i != j) {
+  let temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+  i++;
+  j--;
+}
 
 console.log(arr);
-console.log(temp);
-
-
 
 
 
