0fbfef6547b2bf8620e3c28ebfe38e21e412c4ce
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-10 18:19:42 +0530

    Selection Sort is added

--- S9_Sorting_Algorithms/app.js
+++ S9_Sorting_Algorithms/app.js
@@ -1,22 +1,44 @@
 //Bubble Sort :-
 
-let arr = [10, 5, 1, 12, 3];
+// let arr = [10, 5, 1, 12, 3];
 
-let n = arr.length;
-console.log(arr);
+// let n = arr.length;
+// console.log(arr);
 
-for (let i = 0; i < n - 1; i++) {
-  for (let j = 0; j < n - 1 - i; j++) {
-    if (arr[j] > arr[j + 1]) {
-      let temp = arr[j];
-      arr[j] = arr[j + 1];
-      arr[j + 1] = temp;
-    }
-  }
-}
+// for (let i = 0; i < n - 1; i++) {
+//   for (let j = 0; j < n - 1 - i; j++) {
+//     if (arr[j] > arr[j + 1]) {
+//       let temp = arr[j];
+//       arr[j] = arr[j + 1];
+//       arr[j + 1] = temp;
+//     }
+//   }
+// }
 
 
-console.log(arr);
+// console.log(arr);
 
 
 //SECTION -  Selection Sort :-
+
+let arr = [10, 5, 1, 12, 3];
+let n = arr.length;
+
+for (let i = 0; i < n - 1; i++) {
+  let minIndex = i;
+  for (let j = i + 1; j < n; j++) {
+    if (arr[minIndex] > arr[j]) {
+      minIndex = j;
+    }
+  };
+
+  if (minIndex != i) {
+    let temp = arr[minIndex];
+    arr[minIndex] = arr[i];
+    arr[i] = temp;
+  };
+};
+
+console.log(arr);
+
+
