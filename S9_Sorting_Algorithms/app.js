409ba3dd9755927c73d96726b478331b3f240f15
yashodip deore <yashodipdeore2006@gmail.com>
2026-03-11 18:03:16 +0530

    Added my version of bubble sort.

--- S9_Sorting_Algorithms/app.js
+++ S9_Sorting_Algorithms/app.js
@@ -1,4 +1,4 @@
-//Bubble Sort :-
+//SECTION -  Bubble Sort :-
 
 // let arr = [10, 5, 1, 12, 3];
 
@@ -15,30 +15,70 @@
 //   }
 // }
 
+// console.log(arr);
+
+//-----------My version -------------
+
+// let arr = [10, 5, 1, 12, 3];
+// let n = arr.length;
+
+
+// for (let i = 0; i < n; i++) {
+//   for (let j = 0; j < n - 1; j++) {
+//     if (arr[j] > arr[i]) {
+//       temp = arr[j];
+//       arr[j] = arr[i];
+//       arr[i] = temp;
+//     }
+//   }
+// }
 
 // console.log(arr);
 
 
+
 //SECTION -  Selection Sort :-
 
-let arr = [10, 5, 1, 12, 3];
-let n = arr.length;
+// let arr = [10, 5, 1, 12, 3];
+// let n = arr.length;
 
-for (let i = 0; i < n - 1; i++) {
-  let minIndex = i;
-  for (let j = i + 1; j < n; j++) {
-    if (arr[minIndex] > arr[j]) {
-      minIndex = j;
-    }
-  };
+// for (let i = 0; i < n - 1; i++) {
+//   let minIndex = i;
+//   for (let j = i + 1; j < n; j++) {
+//     if (arr[minIndex] > arr[j]) {
+//       minIndex = j;
+//     }
+//   };
 
-  if (minIndex != i) {
-    let temp = arr[minIndex];
-    arr[minIndex] = arr[i];
-    arr[i] = temp;
-  };
-};
+//   if (minIndex != i) {
+//     let temp = arr[minIndex];
+//     arr[minIndex] = arr[i];
+//     arr[i] = temp;
+//   };
+// };
 
-console.log(arr);
+// console.log(arr);
 
 
+
+
+
+//SECTION - Insertion Sort
+
+// let arr = [10, 5, 1, 12, 3];
+// let n = arr.length;
+
+// for (let i = 1; i < n; i++) {
+//   let key = arr[i];
+//   let j = i - 1;
+
+//   while (j >= 0 && arr[j] > key) {
+//     arr[j + 1] = arr[j];
+//     j--;
+//   }
+//   arr[j + 1] = key;
+
+
+// }
+
+// console.log(arr);
